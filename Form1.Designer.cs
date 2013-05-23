acc3929c06db71b4b29b31a1592e8be83c10ad05
Anthony Jorion <anthony.jorion@gmail.com>
2013-05-23 17:26:06 +0200

    MàJ

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -30,7 +30,14 @@
         {
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.button1 = new System.Windows.Forms.Button();
-            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
             this.SuspendLayout();
             // 
             // textBox1
@@ -50,25 +57,69 @@
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
-            // listBox1
+            // tabControl1
             // 
-            this.listBox1.FormattingEnabled = true;
-            this.listBox1.Location = new System.Drawing.Point(12, 89);
-            this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(510, 264);
-            this.listBox1.TabIndex = 2;
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Location = new System.Drawing.Point(12, 95);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(526, 271);
+            this.tabControl1.TabIndex = 2;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.listView1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(518, 245);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "tabPage1";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.textBox2);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(518, 245);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "tabPage2";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // listView1
+            // 
+            this.listView1.Location = new System.Drawing.Point(5, 4);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(510, 241);
+            this.listView1.TabIndex = 3;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(6, 6);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(506, 236);
+            this.textBox2.TabIndex = 0;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(534, 363);
-            this.Controls.Add(this.listBox1);
+            this.ClientSize = new System.Drawing.Size(550, 378);
+            this.Controls.Add(this.tabControl1);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.textBox1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage2.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -78,7 +129,11 @@
 
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.TextBox textBox2;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -20,6 +20,19 @@ namespace Outils
 
         private void Form1_Load(object sender, EventArgs e)
         {
+            this.listView1.View = System.Windows.Forms.View.Details;
+
+            ColumnHeader columnheader0 = new ColumnHeader();
+            columnheader0.Text = "CP";
+
+            ColumnHeader columnheader1 = new ColumnHeader();
+            columnheader1.Text = "Ville";
+            columnheader1.Width = 150;
+            columnheader1.TextAlign = HorizontalAlignment.Left;
+
+           
+            listView1.Columns.Add(columnheader0);
+            listView1.Columns.Add(columnheader1);
 
         }
 
@@ -32,7 +45,9 @@ namespace Outils
         */
         private void button1_Click(object sender, EventArgs e)
         {
-            System.Net.WebRequest obj = System.Net.HttpWebRequest.Create("http://www.codesposte.fr/index.php?recherche=vannes");
+            listView1.Items.Clear();
+
+            System.Net.WebRequest obj = System.Net.HttpWebRequest.Create("http://www.codespostaux.com/home/r.php?q=" + textBox1.Text + "&Pays=FR");
 
             System.Net.WebResponse objResponse = obj.GetResponse();
 
@@ -43,23 +58,24 @@ namespace Outils
                 stream = new System.IO.StreamReader(objResponse.GetResponseStream());
 
                 string cleanedtext = HtmlSanitizer.getText(stream.ReadToEnd());
-                //textBox2.Text = cleanedtext;
-                Regex cpregex = new Regex("\\d{5}");
-                Regex villeregex = new Regex("[A-Z].");
+                textBox2.Text = cleanedtext;
+
+                Regex cpregex = new Regex(@"(\d{5}?)([A-Z\s]*)");
+                //Regex villeregex = new Regex(@"[A-Z\s]*");
 
                 var cpmatches = cpregex.Matches(cleanedtext);
-                var villematches = villeregex.Matches(cleanedtext);
+                //var villematches = villeregex.Matches(cleanedtext);
 
+                //Console.Write(cleanedtext);
 
-                foreach (Match match in cpmatches)
+                MatchCollection matches = cpregex.Matches(cleanedtext);
+
+                for (int i = 0; i != matches.Count; ++i)
                 {
-                    Console.Write(match.Value + "\n");
+                    Console.Write(matches[i].Groups["1"].Value + " - " + matches[i].Groups["2"].Value + "\n");
                 }
-                    foreach (Match vmatch in villematches)
-                    {
-                        listBox1.Text = vmatch.Value + "\n";
-                    }
                 
+                    
 
 
             }
